4ba9208a4a33fb5b862e76abc49daca481777fad
EdgardoP <edgardoportillo05@gmail.com>
2022-04-01 01:47:18 -0600

    Control Inventario, Usuarios, Ventas, Catalogo, estilos

--- src/controllers/dashboardControlador.js
+++ src/controllers/dashboardControlador.js
@@ -4,7 +4,51 @@ controller.menuEmpleados = (req, res) => {
     res.render('menuEmpleados')
 }
 controller.controlUsuarios = (req, res) => {
-    res.render('controlUsuarios');
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM the_locker.usuario;', (err, usuarios) => {
+            res.render('controlUsuarios', {
+                data: usuarios
+            });
+        })
+    })
+}
+
+controller.controlInventario = (req, res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM the_locker.producto;', (err, producto) => {
+            res.render('controlInventario', {
+                data: producto
+            })
+        })
+    })
+}
+
+controller.controlVentas = (req, res) => {
+
+    const query = "SELECT noTicket, fechaVenta,nombreProducto, " +
+        "nombreTarjeta, noTarjeta,nombreCompleto, direcEnvio, " +
+        "cantidadProducto,precioProducto, cantidadProducto * precioProducto As Total " +
+        "FROM ventas " +
+        "inner join carrito on fkIdCarrito = idCarrito " +
+        "inner join producto on fkIdProducto = idProducto ";
+
+    req.getConnection((err, conn) => {
+        conn.query(`${query}`, (err, ventas) => {
+            res.render('controlVentas', {
+                data: ventas
+            })
+        })
+    })
+}
+
+controller.controlCatalogo = (req, res) => {
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM the_locker.producto;', (err, producto) => {
+            res.render('controlCatalogo', {
+                data: producto
+            })
+        })
+    })
 }
 
 
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -4,5 +4,7 @@ const dashboardControlador = require('../controllers/dashboardControlador');
 
 router.get('/', dashboardControlador.menuEmpleados);
 router.get('/controlUsuarios', dashboardControlador.controlUsuarios);
-
+router.get('/controlInventario', dashboardControlador.controlInventario);
+router.get('/controlVentas', dashboardControlador.controlVentas);
+router.get('/controlCatalogo',dashboardControlador.controlCatalogo);
 module.exports = router;
